03870ada853dab01742afa6878d73acd4e507aef
Ethan Reynolds <104602809+EthanReynoldsCoding@users.noreply.github.com>
2023-10-29 22:45:41 -0400

    Update index.js
    added print and filter button. Print button has full functionality. Filter button has none as of this commit

--- src/layouts/data-tables/index.js
+++ src/layouts/data-tables/index.js
@@ -1,14 +1,15 @@
-import { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Card from "@mui/material/Card";
 import VuiBox from "components/VuiBox";
 import VuiTypography from "components/VuiTypography";
 import VuiButton from "components/VuiButton";
+import VuiSelect from "components/VuiSelect"; // Import the Select component
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import DataTable from "examples/Tables/DataTable";
 import dataTableData from "layouts/data-tables/data/dataTableData";
-import NewSale from "layouts/data-tables/components/Sale"; // Import the NewSale component
+import NewSale from "layouts/data-tables/components/Sale";
 import { supabase } from "supabaseClient.js";
 import { useAuth } from "hooks/Auth";
 
@@ -16,6 +17,8 @@ function TablesLayout() {
   const [sales, setSales] = useState([]);
   const [open, setOpen] = useState(false);
   const [dataUpdated, setDataUpdates] = useState(false);
+  const [selectedMonth, setSelectedMonth] = useState("");
+  const [isFilterDisabled, setIsFilterDisabled] = useState(true); // Initialize as disabled
 
   const { user } = useAuth();
   const userId = user?.id;
@@ -29,22 +32,70 @@ function TablesLayout() {
     setDataUpdates(true);
   }, []);
 
-  const getSales = useCallback(async () => {
-    const { data } = await supabase.from("sales").select().eq("user_id", userId);
-    setSales(data);
-  }, [userId]);
+  const handlePrint = () => {
+    const printWindow = window.open("", "", "width=600,height=600");
+    printWindow.document.open();
+    printWindow.document.write(`
+      <html>
+        <head>
+          <title>Print</title>
+        </head>
+        <body>
+          <style>
+            @media print {
+              .no-print {
+                display: none;
+              }
+            }
+          </style>
+          <div class="card-print">${document.querySelector(".card-print").innerHTML}</div>
+        </body>
+      </html>
+    `);
+    printWindow.document.close();
+    printWindow.print();
+    printWindow.close();
+  };
 
+  const getSales = useCallback(async () => {
+    const query = selectedMonth
+      ? supabase.from("sales").select().eq("user_id", userId).eq("date", selectedMonth)
+      : supabase.from("sales").select().eq("user_id", userId);
+  
+    const { data } = await query;
+  
+    // Don't change the original dates, only format for display
+    const formattedData = data.map((sale) => ({
+      ...sale,
+      dateFormatted: new Date(sale.date).toLocaleDateString("en-US")
+    }));
+  
+    setSales(formattedData || []);
+  }, [userId, selectedMonth]);
+  
   useEffect(() => {
     getSales();
     setDataUpdates(false);
   }, [dataUpdated, getSales]);
+  
+
+  const handleFilterByMonth = () => {
+    setIsFilterDisabled(true);
+    getSales();
+  };
+
+  // Function to handle month selection
+  const handleMonthSelect = (event) => {
+    setSelectedMonth(event.target.value);
+    setIsFilterDisabled(false); // Enable the filter button
+  };
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <VuiBox pt={6} pb={3}>
         <VuiBox mb={3} position="relative">
-          <Card>
+          <Card className="card-print">
             <VuiBox p={3} pl={0} lineHeight={1}>
               <VuiTypography variant="h5" fontWeight="medium" color="white">
                 Vehicle Sales
@@ -54,15 +105,47 @@ function TablesLayout() {
               </VuiTypography>
             </VuiBox>
             <VuiBox position="absolute" top={0} right={0} m={3}>
-              {/* "Add Sale" Button */}
-              <VuiButton variant="contained" color="primary" onClick={handleClickOpen}>
-                Add Sale
-              </VuiButton>
+              <div style={{ marginBottom: "10px", display: "flex", flexDirection: "row" }}>
+                <VuiButton
+                  variant="contained"
+                  color="primary"
+                  onClick={handleFilterByMonth}
+                  disabled={isFilterDisabled}
+                >
+                  Filter by Month
+                </VuiButton>
+                <VuiButton variant="contained" color="primary" onClick={handleClickOpen} style={{ marginLeft: "10px" }}>
+                  Add Sale
+                </VuiButton>
+                <VuiButton variant="contained" color="primary" onClick={handlePrint} style={{ marginLeft: "10px" }}>
+                  Print
+                </VuiButton>
+              </div>
+              <div style={{ marginBottom: "10px" }}>
+                {/* Select component for month selection */}
+                <VuiSelect
+                  label="Select Month"
+                  value={selectedMonth}
+                  onChange={handleMonthSelect}
+                  sx={{ minWidth: "150px" }}
+                >
+                  <option value="">Select Month</option>
+                  <option value="01">January</option>
+                  <option value="02">February</option>
+                  <option value="03">March</option>
+                  <option value="04">April</option>
+                  <option value="05">May</option>
+                  <option value="06">June</option>
+                  <option value="07">July</option>
+                  <option value="08">August</option>
+                  <option value="09">September</option>
+                  <option value="10">October</option>
+                  <option value="11">November</option>
+                  <option value="12">December</option>
+                </VuiSelect>
+              </div>
             </VuiBox>
-            {/* {isFormOpen && <NewSale isFormOpen={isFormOpen} setIsFormOpen={setIsFormOpen} />}{" "} */}
             <NewSale open={open} handleClose={handleClose} setDataUpdates={setDataUpdates} />
-
-            {/* Render NewSale when isFormOpen is true */}
             <DataTable
               table={{ columns: dataTableData.columns, rows: [...dataTableData["rows"], ...sales] }}
               canSearch
@@ -77,3 +160,5 @@ function TablesLayout() {
 }
 
 export default TablesLayout;
+
+
